287a9d85804ab09425aa3f6a41cab5033738677e
hduarte <29019005+Bacmel@users.noreply.github.com>
2022-09-14 16:20:38 +0200

    feat: Add position of the feet

--- solo12_viewer/solo12_viewer.py
+++ solo12_viewer/solo12_viewer.py
@@ -40,7 +40,7 @@ yaml = '/home/aschroeter/devel/odri_control_interface/demos/config_solo12.yaml'
 
 # Goal frame
 frame_name = 'base_link'
-frame_goal = pin.SE3(np.eye(3), np.array([.1, .0, 0]))
+frame_goal = pin.SE3(np.eye(3), np.array([.05, .0, -0.1]))
 
 # Physical constants
 dt = 1e-3 # in [s]
@@ -167,13 +167,39 @@ class Solo12Viewer(Node):
         runningCM.addCost('xReg', xCost, 1e-4)
         
         
+        #
+        frameid = self.pin_robot.model.getFrameId('FL_FOOT')
+        frameRes = crocoddyl.ResidualModelFrameTranslation(state, frameid, np.array([0.1946, 0.14695, -0.2]))
+        goalCost = crocoddyl.CostModelResidual(state, frameRes)
+        runningCM.addCost('FL_FOOT', goalCost, 1)
+        terminalCM.addCost('FL_FOOT', goalCost, 1)
+        
+        frameid = self.pin_robot.model.getFrameId('FR_FOOT')
+        frameRes = crocoddyl.ResidualModelFrameTranslation(state, frameid, np.array([0.1946, -0.14695, -0.3]))
+        goalCost = crocoddyl.CostModelResidual(state, frameRes)
+        runningCM.addCost('FR_FOOT', goalCost, 1)
+        terminalCM.addCost('FR_FOOT', goalCost, 1)
+
+        frameid = self.pin_robot.model.getFrameId('HL_FOOT')
+        frameRes = crocoddyl.ResidualModelFrameTranslation(state, frameid, np.array([-0.1946, 0.14695, -0.2]))
+        goalCost = crocoddyl.CostModelResidual(state, frameRes)
+        runningCM.addCost('HL_FOOT', goalCost, 1)
+        terminalCM.addCost('HL_FOOT', goalCost, 1)
+
+        frameid = self.pin_robot.model.getFrameId('HR_FOOT')
+        frameRes = crocoddyl.ResidualModelFrameTranslation(state, frameid, np.array([-0.1946, -0.14695, -0.2]))
+        goalCost = crocoddyl.CostModelResidual(state, frameRes)
+        runningCM.addCost('HR_FOOT', goalCost, 1)
+        terminalCM.addCost('HR_FOOT', goalCost, 1)
+        
+        
         
         ## Goal Cost
-        frameid = self.pin_robot.model.getFrameId(frame_name)
-        frameRes = crocoddyl.ResidualModelFramePlacement(state, frameid, frame_goal)
-        goalCost = crocoddyl.CostModelResidual(state, frameRes)
-        runningCM.addCost(frame_name, goalCost, 1)
-        terminalCM.addCost(frame_name, goalCost, 1)
+        #frameid = self.pin_robot.model.getFrameId(frame_name)
+        #frameRes = crocoddyl.ResidualModelFramePlacement(state, frameid, frame_goal)
+        #goalCost = crocoddyl.CostModelResidual(state, frameRes)
+        #runningCM.addCost(frame_name, goalCost, 1)
+        #terminalCM.addCost(frame_name, goalCost, 1)
 
         # Creation Action Model
         actuationM = crocoddyl.ActuationModelFull(state)
