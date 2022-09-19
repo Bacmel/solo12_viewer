b573ad5ba802a2e5a962dfa9165fe09dc2baa4a0
hduarte <29019005+Bacmel@users.noreply.github.com>
2022-09-19 18:29:31 +0200

    ref: Update trajectory generator

--- solo12_viewer/trajectory_generator.py
+++ solo12_viewer/trajectory_generator.py
@@ -4,6 +4,7 @@ import numpy as np
 import pinocchio as pin
 from pinocchio.utils import *
 
+
 ## LISTS OF DATA
 feet_name_list = ['FL_FOOT', 'FR_FOOT', 'HL_FOOT', 'HR_FOOT']
 feet_pos_list = [np.array([0.1946, 0.14695, -0.2]),
@@ -22,10 +23,14 @@ pin.framesForwardKinematics(robot.model, robot.data, q0)
 
 ## Physical constants
 dt = 1e-3 # in [s]
+
 T = 1000 # knots
 
-## Creation state
+## Creation state / actuation
 state = croco.StateMultibody(robot.model)
+actuation = croco.ActuationModelFull(state)
+
+actions_model = []
 
 ########################################## COST
 
@@ -36,60 +41,153 @@ terminal_cost_model = croco.CostModelSum(state)
 ## Command Regulation Cost
 u_res = croco.ResidualModelControl(state)
 u_cost = croco.CostModelResidual(state, u_res)
-running_cost_model.addCost('uReg', u_cost, 1e-4)
         
 ## State Regulation Cost 
 x_res = croco.ResidualModelState(state, x0)
 x_cost = croco.CostModelResidual(state, x_res)
-running_cost_model.addCost('xReg', x_cost, 1e-3)
 
-## 
 
-     
-## Feet Goal Cost
-# for idx, foot_name in enumerate(feet_name_list) :
-#    foot_id = robot.model.getFrameId(foot_name)
-#    foot_pos = feet_pos_list[idx]
-#    foot_res = croco.ResidualModelFrameTranslation(state, foot_id, foot_pos)
-#    foot_goal_cost = croco.CostModelResidual(state, foot_res)
-#    running_cost_model.addCost(foot_name, foot_goal_cost, 1)
-#    terminal_cost_model.addCost(foot_name, foot_goal_cost, 1)      
-        
-## Base Link Goal Cost
-frame_goal = pin.SE3(rpyToMatrix(np.array([0, 0, np.deg2rad(20)])), np.array([.0, .0, 0.2]))
+###################################################################################### Action 1 Preparation 5s
+action_1_T = int(2/dt)
+print("action_1_T: ", action_1_T)
+## COSTS
+action_1_cost_model  = croco.CostModelSum(state)
+# Base Link Goal Cost
+frame_goal = pin.SE3(rpyToMatrix(np.array([0, 0, 0])), np.array([.0, .0, 0.1]))
 base_link_id  = robot.model.getFrameId('base_link')
 base_link_res = croco.ResidualModelFramePlacement(state, base_link_id, frame_goal)
 base_link_goal_cost = croco.CostModelResidual(state, base_link_res)
-terminal_cost_model.addCost('base_link', base_link_goal_cost, 1)
-
-#################################################################################### CONTACT
-
-## Creation Contact
-contact_model = croco.ContactModelMultiple(state)
-
-## Feet Contact         
+action_1_cost_model.addCost('base_link', base_link_goal_cost, 10)
+# Reg cost
+action_1_cost_model.addCost('uReg', u_cost, 1e-4)
+action_1_cost_model.addCost('xReg', x_cost, 1e-3)
+## CONTACTS
+action_1_contact_model = croco.ContactModelMultiple(state)
+# Feet Contact         
 for idx, foot_name in enumerate(feet_name_list) :
     foot_id = robot.model.getFrameId(foot_name)
     foot_pos = feet_pos_list[idx]
     foot_contact = croco.ContactModel3D(state, foot_id, foot_pos)
-    contact_model.addContact(foot_name, foot_contact)
+    action_1_contact_model.addContact(foot_name, foot_contact)
+## Integrated Action
+action_1_dif_model = croco.DifferentialActionModelContactFwdDynamics(state, actuation, action_1_contact_model, action_1_cost_model)
+action_1_model = croco.IntegratedActionModelEuler(action_1_dif_model, dt)
 
-#################################################################################### Action Model
+actions_model = actions_model + [action_1_model]*action_1_T
 
-## Creation Action Model
-actuation_model = croco.ActuationModelFull(state)
 
-running_dif_model = croco.DifferentialActionModelContactFwdDynamics(state, actuation_model, contact_model, running_cost_model)
-running_model = croco.IntegratedActionModelEuler(running_dif_model, dt)
+###################################################################################### Action 2 Push
+action_2_T = int(.5/dt)
+print("action_2_T: ", action_2_T)
+## COSTS
+action_2_cost_model  = croco.CostModelSum(state)
+# Reg cost
+action_2_cost_model.addCost('uReg', u_cost, 1e-4)
+action_2_cost_model.addCost('xReg', x_cost, 1e-4)
+## CONTACTS
+action_2_contact_model = croco.ContactModelMultiple(state)
+# Feet Contact         
+for idx, foot_name in enumerate(feet_name_list) :
+    foot_id = robot.model.getFrameId(foot_name)
+    foot_pos = feet_pos_list[idx]
+    foot_contact = croco.ContactModel3D(state, foot_id, foot_pos)
+    action_2_contact_model.addContact(foot_name, foot_contact)
+## Integrated Action
+#action_2_dif_model = croco.DifferentialActionModelContactFwdDynamics(state, actuation, action_2_contact_model, action_2_cost_model)
+action_2_dif_model = croco.DifferentialActionModelFreeFwdDynamics(state, actuation, action_2_cost_model)
+action_2_model = croco.IntegratedActionModelEuler(action_2_dif_model, dt)
 
-terminal_dif_model = croco.DifferentialActionModelContactFwdDynamics(state, actuation_model, contact_model, terminal_cost_model)
-terminal_model = croco.IntegratedActionModelEuler(terminal_dif_model, 0)    
+actions_model = actions_model + [action_2_model]*action_2_T
+
+
+###################################################################################### Action 3 Fly
+action_3_T = int(0.05/dt)
+print("action_3_T: ", action_3_T)
+## COSTS
+action_3_cost_model  = croco.CostModelSum(state)
+# Base Link Goal Cost
+frame_goal = pin.SE3(rpyToMatrix(np.array([0, 0, 0])), np.array([.0, .0, .4]))
+base_link_id  = robot.model.getFrameId('base_link')
+base_link_res = croco.ResidualModelFramePlacement(state, base_link_id, frame_goal)
+base_link_goal_cost = croco.CostModelResidual(state, base_link_res)
+action_3_cost_model.addCost('base_link', base_link_goal_cost, 100)
+# Reg cost
+action_3_cost_model.addCost('uReg', u_cost, 1e-4)
+action_3_cost_model.addCost('xReg', x_cost, 1e-4)
+## Integrated Action
+action_3_dif_model = croco.DifferentialActionModelFreeFwdDynamics(state, actuation, action_3_cost_model)
+action_3_model = croco.IntegratedActionModelEuler(action_3_dif_model, dt)
+
+actions_model = actions_model + [action_3_model]*action_3_T
+
+###################################################################################### Action 4 In air
+action_4_T = int(.5/dt)
+print("action_4_T: ", action_4_T)
+## COSTS
+action_4_cost_model  = croco.CostModelSum(state)
+# Reg cost
+action_4_cost_model.addCost('uReg', u_cost, 1e-4)
+action_4_cost_model.addCost('xReg', x_cost, 1e-4)
+## CONTACTS
+action_4_contact_model = croco.ContactModelMultiple(state)
+# Feet Contact         
+for idx, foot_name in enumerate(feet_name_list) :
+    foot_id = robot.model.getFrameId(foot_name)
+    foot_pos = feet_pos_list[idx]
+    foot_contact = croco.ContactModel3D(state, foot_id, foot_pos)
+    action_4_contact_model.addContact(foot_name, foot_contact)
+## Integrated Action
+#action_4_dif_model = croco.DifferentialActionModelContactFwdDynamics(state, actuation, action_4_contact_model, action_4_cost_model)
+action_4_dif_model = croco.DifferentialActionModelFreeFwdDynamics(state, actuation, action_4_cost_model)
+action_4_model = croco.IntegratedActionModelEuler(action_4_dif_model, dt)
+
+actions_model = actions_model + [action_4_model]*action_4_T
+
+###################################################################################### Action 5 Landing## Creation Contact
+action_5_T = int(1/dt)
+print("action_5_T: ", action_5_T)
+## COSTS
+action_5_cost_model  = croco.CostModelSum(state)
+# Reg cost
+action_5_cost_model.addCost('uReg', u_cost, 1e-4)
+action_5_cost_model.addCost('xReg', x_cost, 1e-2)
+## CONTACTS
+action_5_contact_model = croco.ContactModelMultiple(state)
+# Feet Contact         
+for idx, foot_name in enumerate(feet_name_list) :
+    foot_id = robot.model.getFrameId(foot_name)
+    foot_pos = feet_pos_list[idx]
+    foot_contact = croco.ContactModel3D(state, foot_id, foot_pos)
+    action_5_contact_model.addContact(foot_name, foot_contact)
+## Integrated Action
+action_5_dif_model = croco.DifferentialActionModelContactFwdDynamics(state, actuation, action_5_contact_model, action_5_cost_model)
+action_5_model = croco.IntegratedActionModelEuler(action_5_dif_model, dt)
+
+actions_model = actions_model + [action_5_model]*action_5_T
+
+###################################################################################### Action Terminal
+## COSTS
+action_terminal_cost_model  = croco.CostModelSum(state)
+# Reg cost
+action_terminal_cost_model.addCost('uReg', u_cost, 1e-4)
+action_terminal_cost_model.addCost('xReg', x_cost, 1e-3)
+## CONTACTS
+action_terminal_contact_model = croco.ContactModelMultiple(state)
+# Feet Contact         
+for idx, foot_name in enumerate(feet_name_list) :
+    foot_id = robot.model.getFrameId(foot_name)
+    foot_pos = feet_pos_list[idx]
+    foot_contact = croco.ContactModel3D(state, foot_id, foot_pos)
+    action_terminal_contact_model.addContact(foot_name, foot_contact)
+## Integrated Action
+action_terminal_dif_model = croco.DifferentialActionModelContactFwdDynamics(state, actuation, action_terminal_contact_model, action_terminal_cost_model)
+action_terminal_model = croco.IntegratedActionModelEuler(action_terminal_dif_model, 0)
 
 
 ## Solve
-problem = croco.ShootingProblem(x0, [running_model]*T, terminal_model)
+problem = croco.ShootingProblem(x0, actions_model, action_terminal_model)
 solver = croco.SolverFDDP(problem)
 solver.solve()
 
 # Save the trajectory
-np.save('base_move.npy', solver.xs)
+np.save('jump_move.npy', solver.xs)
