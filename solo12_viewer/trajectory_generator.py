a896c4f4f10492816f743aa68603dd0d2b65780f
hduarte <29019005+Bacmel@users.noreply.github.com>
2022-09-30 18:32:22 +0200

    fix: Update trajectory generator

--- solo12_viewer/trajectory_generator.py
+++ solo12_viewer/trajectory_generator.py
@@ -12,36 +12,41 @@ import matplotlib.pyplot as plt
 feet_name_list = ['FL_FOOT', 'FR_FOOT', 'HL_FOOT', 'HR_FOOT']
 
 lower_legs_name_list = ['FL_LOWER_LEG', 'FR_LOWER_LEG', 'HL_LOWER_LEG', 'HR_LOWER_LEG']
-lower_legs_pos_list = [-0.8, -0.8, 0.8, 0.8]
+lower_legs_pos_list = [0.2, 0.2, -0.2, -0.2]
 
 upper_legs_name_list = ['FL_UPPER_LEG', 'FR_UPPER_LEG', 'HL_UPPER_LEG', 'HR_UPPER_LEG']
-upper_legs_pos_list = [0.8, 0.8, -0.8, -0.8]
+upper_legs_pos_list = [-0.3, -0.3, 0.3, 0.3]
 
 
 shoulders_name_list = ['FL_SHOULDER', 'FR_SHOULDER', 'HL_SHOULDER', 'HR_SHOULDER']
 
-feet_pos_list = [np.array([0.1946, 0.14695, 0.0]),
-                 np.array([0.1946, -0.14695, 0.0]),
-                 np.array([-0.1946, 0.14695, 0.0]),
-                 np.array([-0.1946, -0.14695, 0.0])]
+# feet_pos_list = [np.array([0.1946, 0.14695, 0.0]),
+#                  np.array([0.1946, -0.14695, 0.0]),
+#                  np.array([-0.1946, 0.14695, 0.0]),
+#                  np.array([-0.1946, -0.14695, 0.0])]
+
+feet_pos_list = [np.array([0.3, 0.14695, 0.0]),
+                 np.array([0.3, -0.14695, 0.0]),
+                 np.array([-0.3, 0.14695, 0.0]),
+                 np.array([-0.3, -0.14695, 0.0])]
 
 ################################################################ Global variables
 robot = example_robot_data.load('solo12')
-robot.model.effortLimit[6:] = np.ones(12) * 0.80
+robot.model.effortLimit[6:] = np.ones(12) * 1.2
 nq = robot.nq
 q0 = robot.q0
 nv = robot.nv
 v0 = robot.v0
 
 q_start = np.array([
-                    0.0, 0.0, 0.50,
+                    0.0, 0.0, 0.07,
                     0.0, 0.0, 0.0, 1.0,
-                    0.0, 0.7, -1.4,
-                    0.0, 0.7, -1.4,
-                    0.0, -0.7, 1.4,
-                    0.0, -0.7, 1.4])
+                    0.0, -2.2, 2.4,
+                    0.0, -2.2, 2.4,
+                    0.0, 2.2, -2.4,
+                    0.0, 2.2, -2.4])
 
-#q0 = q_start
+q0 = q_start
 
 x0 = np.concatenate([q0, v0])
 
@@ -73,7 +78,7 @@ x_res = croco.ResidualModelState(state, x0, actuation.nu)
 x_cost = croco.CostModelResidual(state, x_res)
 
 
-def feet_contact_cost(contact_model, cost_model):
+def feet_contact_cost(contact_model, cost_model, w=10):
     motion_zero = pin.Motion(np.array([.0, .0, .0]), np.array([.0, .0, .0]))
     for idx, foot_name in enumerate(feet_name_list):
         foot_id = robot.model.getFrameId(foot_name)
@@ -82,45 +87,45 @@ def feet_contact_cost(contact_model, cost_model):
         contact_model.addContact(foot_name, foot_contact)
         foot_pos_res = croco.ResidualModelFrameTranslation(state, foot_id, foot_pos, actuation.nu)
         foot_pos_cost = croco.CostModelResidual(state, foot_pos_res)
-        cost_model.addCost(foot_name+' position', foot_pos_cost, 10)
+        cost_model.addCost(foot_name+' position', foot_pos_cost, w)
         foot_vel_res = croco.ResidualModelFrameVelocity(state, foot_id, motion_zero, pin.LOCAL, actuation.nu)
         foot_vel_cost = croco.CostModelResidual(state, foot_vel_res)
-        cost_model.addCost(foot_name+' velocity', foot_vel_cost, 10)
+        cost_model.addCost(foot_name+' velocity', foot_vel_cost, w)
 
-def shoulders_cost(cost_model):
+def shoulders_cost(cost_model, w=100):
     rotation_zero = rpyToMatrix(np.array([0, 0, 0]))
     for shoulder_name in shoulders_name_list:
         shoulder_id = robot.model.getFrameId(shoulder_name)
         shoulder_res = croco.ResidualModelFrameRotation(state, shoulder_id, rotation_zero, actuation.nu)
         shoulder_cost = croco.CostModelResidual(state, shoulder_res)
-        cost_model.addCost(shoulder_name+' rotation', shoulder_cost, 100)
+        cost_model.addCost(shoulder_name+' rotation', shoulder_cost, w) 
 
-def upper_leg_cost(cost_model):  
+def upper_leg_cost(cost_model, w=1000):  
     for idx, upper_leg_name in enumerate(upper_legs_name_list):
         rotation_zero = rpyToMatrix(np.array([0, upper_legs_pos_list[idx], 0]))
         upper_leg_id = robot.model.getFrameId(upper_leg_name)
         upper_leg_res = croco.ResidualModelFrameRotation(state, upper_leg_id, rotation_zero, actuation.nu)
         upper_leg_cost = croco.CostModelResidual(state, upper_leg_res)
-        cost_model.addCost(upper_leg_name+' rotation', upper_leg_cost, 100)
+        cost_model.addCost(upper_leg_name+' rotation', upper_leg_cost, w)
 
-def lower_leg_cost(cost_model):
+def lower_leg_cost(cost_model, w=1000):
     for idx, lower_leg_name in enumerate(lower_legs_name_list):
         rotation_zero = rpyToMatrix(np.array([0, lower_legs_pos_list[idx], 0]))
         lower_leg_id = robot.model.getFrameId(lower_leg_name)
         lower_leg_res = croco.ResidualModelFrameRotation(state, lower_leg_id, rotation_zero, actuation.nu)
         lower_leg_cost = croco.CostModelResidual(state, lower_leg_res)
-        cost_model.addCost(lower_leg_name+' rotation', lower_leg_cost, 100)
+        cost_model.addCost(lower_leg_name+' rotation', lower_leg_cost, w)
     
 
 ###################################################################################### Action 1 Preparation
-action_1_T = int(0.2/dt)  #0.25
+action_1_T = int(0.3/dt)  #0.135
 
 ## COSTS
 action_1_cost_model  = croco.CostModelSum(state, actuation.nu)
 
 # Reg cost
-action_1_cost_model.addCost('uReg', u_contact_cost, 1e-3)
-action_1_cost_model.addCost('xReg', x_cost, 1e-0)
+action_1_cost_model.addCost('uReg', u_contact_cost, 1e3)
+action_1_cost_model.addCost('xReg', x_cost, 1e-2)
 
 # ## Base Link Start Position
 # frame_goal = np.array([.0, .0, 0.05])
@@ -132,6 +137,8 @@ rotation_goal = rpyToMatrix(np.array([0, 0, 0]))
 base_link_rotation_res = croco.ResidualModelFrameRotation(state, base_link_id, rotation_goal, actuation.nu)
 base_link_rotation_goal_cost = croco.CostModelResidual(state, base_link_rotation_res)
 action_1_cost_model.addCost('base_link rotation', base_link_rotation_goal_cost, 1e4)
+
+#shoulders_cost(action_1_cost_model, 1e2)
 
 ## CONTACTS
 action_1_contact_model = croco.ContactModelMultiple(state, actuation.nu)
@@ -152,7 +159,7 @@ action_2_cost_model  = croco.CostModelSum(state, actuation.nu)
 
 # Reg cost
 action_2_cost_model.addCost('uReg', u_contact_cost, 1e-3)
-action_2_cost_model.addCost('xReg', x_cost, 1e-4)
+#action_2_cost_model.addCost('xReg', x_cost, 1e-4)
 
 # # Base Link Goal Cost
 # motion_goal = pin.Motion(np.array([.0, .0, 3]), np.array([.0, .0, .0]))
@@ -175,25 +182,29 @@ feet_contact_cost(action_2_contact_model, action_2_cost_model)
 action_2_dif_model = croco.DifferentialActionModelContactFwdDynamics(state, actuation, action_2_contact_model, action_2_cost_model)
 action_2_model = croco.IntegratedActionModelEuler(action_2_dif_model, dt)
 
-actions_model = actions_model + [action_2_model]*action_2_T
+#actions_model = actions_model + [action_2_model]*action_2_T
 
 
 ###################################################################################### Action 3 Flying
-action_3_T = int(0.25/dt)    #0.30
+action_3_T = int(0.3/dt)    #0.30
 
 ## COSTS
 action_3_cost_model  = croco.CostModelSum(state, actuation.nu)
 
 # Reg cost
-action_3_cost_model.addCost('uReg', u_free_cost, 1e-2)
-action_3_cost_model.addCost('xReg', x_cost, 1e-1)
+action_3_cost_model.addCost('uReg', u_free_cost, 1e0)
+action_3_cost_model.addCost('xReg', x_cost, 1e1)
 
 #shoulders_cost(action_3_cost_model)
+#shoulders_cost(action_3_cost_model)
+shoulders_cost(action_3_cost_model, 1e3)
+upper_leg_cost(action_3_cost_model, 1e2)
+lower_leg_cost(action_3_cost_model, 1e2)
 
 rotation_goal = rpyToMatrix(np.array([0, 0, 0]))
 base_link_rotation_res = croco.ResidualModelFrameRotation(state, base_link_id, rotation_goal, actuation.nu)
 base_link_rotation_goal_cost = croco.CostModelResidual(state, base_link_rotation_res)
-action_3_cost_model.addCost('base_link rotation', base_link_rotation_goal_cost, 1e4)
+action_3_cost_model.addCost('base_link rotation', base_link_rotation_goal_cost, 1e3)
 
 
 ## Integrated Action
@@ -248,14 +259,14 @@ action_terminal_cost_model  = croco.CostModelSum(state, actuation.nu)
 
 # Reg cost
 #action_terminal_cost_model.addCost('uReg', u_contact_cost, 1e-4)
-action_terminal_cost_model.addCost('xReg', x_cost, 1e-0)
+action_terminal_cost_model.addCost('xReg', x_cost, 1e2)
 
 shoulders_cost(action_terminal_cost_model)
 upper_leg_cost(action_terminal_cost_model)
 lower_leg_cost(action_terminal_cost_model)
 
 # terminal position
-terminal_goal = np.array([0, 0, 0.50])
+terminal_goal = np.array([0.0, 0, 1.0])
 base_link_pos_res = croco.ResidualModelFrameTranslation(state, base_link_id, terminal_goal, actuation.nu)
 base_link_pos_goal_cost = croco.CostModelResidual(state, base_link_pos_res)
 action_terminal_cost_model.addCost('base_link translation', base_link_pos_goal_cost, 1e4)
